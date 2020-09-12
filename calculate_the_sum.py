ff12e0520b61276fac959dffcb14833a83abad67
Sergiy Bagdasar <sergio17949@gmail.com>
2020-09-12 12:51:45 +0100

    Writing a list of numbers to a file.

--- calculate_the_sum.py
+++ calculate_the_sum.py
@@ -7,3 +7,8 @@ def read_numbers_from_file():
 
 def calculate_the_sum_of_evens_in_a_list(list_of_numbers):
     return sum([n for n in list_of_numbers if not n % 2])
+
+
+def write_list_of_numbers_to_file(list_of_numbers):
+    file = open("output.txt", "w")
+    file.write(", ".join(", ".join(str(n) for n in list_of_numbers)))
