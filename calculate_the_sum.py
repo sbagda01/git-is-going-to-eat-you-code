5d75fee5eac43ce3e0d725bac05545e77a491672
Sergiy Bagdasar <sergio17949@gmail.com>
2020-09-14 21:49:08 +0100

    Calculating the average from a list of numbers.

--- calculate_the_sum.py
+++ calculate_the_sum.py
@@ -12,3 +12,6 @@ def calculate_the_sum_of_evens_in_a_list(list_of_numbers):
 def write_list_of_numbers_to_file(list_of_numbers):
     file = open("output.txt", "w")
     file.write(", ".join(", ".join(str(n) for n in list_of_numbers)))
+
+def calculate_the_average_of_numbers_in_a_list(list_of_numbers):
+    return sum(list_of_numbers) / len(list_of_numbers)
