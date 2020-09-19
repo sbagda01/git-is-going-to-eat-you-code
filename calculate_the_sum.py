815e2f986f1c735c9cb2d6ba05680a416379075c
Sergiy Bagdasar <sergio17949@gmail.com>
2020-09-19 09:51:06 +0100

    Writing the average to output.txt.

--- calculate_the_sum.py
+++ calculate_the_sum.py
@@ -11,7 +11,10 @@ def calculate_the_sum_of_evens_in_a_list(list_of_numbers):
 
 def write_list_of_numbers_to_file(list_of_numbers):
     file = open("output.txt", "w")
-    file.write(", ".join(", ".join(str(n) for n in list_of_numbers)))
+    list_of_numbers_as_string = ", ".join(", ".join(str(n) for n in list_of_numbers))
+    average_as_string = "Avg:" + str(calculate_the_average_of_numbers_in_a_list(list_of_numbers))
+    output_string = list_of_numbers_as_string + "\n" + average_as_string
+    file.write(output_string)
 
 def calculate_the_average_of_numbers_in_a_list(list_of_numbers):
     return sum(list_of_numbers) / len(list_of_numbers)
