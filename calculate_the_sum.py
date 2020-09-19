94cc0b403628c310ef07c906ecd17ad9afbf2f6e
Sergiy Bagdasar <sergio17949@gmail.com>
2020-09-19 10:36:13 +0100

    Write the sum of evens to output.txt.

--- calculate_the_sum.py
+++ calculate_the_sum.py
@@ -11,4 +11,6 @@ def calculate_the_sum_of_evens_in_a_list(list_of_numbers):
 
 def write_list_of_numbers_to_file(list_of_numbers):
     file = open("output.txt", "w")
-    file.write(", ".join(", ".join(str(n) for n in list_of_numbers)))
+    sum_of_evens_as_string = str(calculate_the_sum_of_evens_in_a_list(list_of_numbers))
+    numbers_as_string = ", ".join(", ".join(str(n) for n in list_of_numbers))
+    file.write(numbers_as_string + "\n" + "Sum of evens: " + sum_of_evens_as_string)
